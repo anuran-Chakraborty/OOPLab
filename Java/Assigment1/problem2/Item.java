7f014b610cee2c7ab37e7c0f21163edaa7e6579c
anuran <ch.anuraan@gmail.com>
2018-03-21 19:51:35 +0530

    Problem2

---
+++ Java/Assigment1/problem2/Item.java
@@ -0,0 +1,54 @@
+package problem2;
+
+import java.util.*;
+class Item
+{
+    //Data members
+    private static int id=0;
+    private int code;
+    private String name;
+    private double rate;
+    private int qty;
+
+    //Constructor
+    public Item(String nm, double r,int q)
+    {
+        name=nm;
+        rate=r;
+        qty=q;
+        code=10000+id;
+        id++;
+    }
+
+    //Set rate
+    public void setRate(double r)
+    {
+        rate=r;
+    }
+
+    //Set quantity
+    public void setQty(int q)
+    {
+        qty=q;
+    }
+    
+    //Get quantity
+    public int getQty()
+    {
+        return qty;
+    }
+    
+    //Get rate
+    public double getRate()
+    {
+        return rate;
+    }
+    
+    //Get code
+    public int getCode()
+    {
+        return code;
+    }
+    
+
+}
